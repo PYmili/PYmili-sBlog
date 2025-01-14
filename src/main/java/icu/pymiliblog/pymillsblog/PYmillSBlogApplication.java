0c32334994550ee3d1978341e347550c3126330a
PYmili <mc2005wj@163.com>
2025-01-14 14:40:16 +0800

    feat(license): add license file.

--- src/main/java/icu/pymiliblog/pymillsblog/PYmillSBlogApplication.java
+++ src/main/java/icu/pymiliblog/pymillsblog/PYmillSBlogApplication.java
@@ -3,13 +3,20 @@ package icu.pymiliblog.pymillsblog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("icu.pymiliblog.pymillsblog")
-public class PYmillSBlogApplication {
+public class PYmillSBlogApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PYmillSBlogApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PYmillSBlogApplication.class);
     }
 
 }
